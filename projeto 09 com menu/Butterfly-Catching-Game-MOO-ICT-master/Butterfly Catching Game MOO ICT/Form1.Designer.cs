2a2bc7039e940f49e76c496d5e3fa6b100ecf478
Carl0sAndre <160773664+Carl0sAndre@users.noreply.github.com>
2025-09-10 18:28:26 -0300

    Add files via upload

---
+++ projeto 09 com menu/Butterfly-Catching-Game-MOO-ICT-master/Butterfly Catching Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Butterfly_Catching_Game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Orange;
+            button1.FlatAppearance.BorderColor = Color.Brown;
+            button1.FlatAppearance.BorderSize = 3;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Comic Sans MS", 14F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(390, 350);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(229, 80);
+            button1.TabIndex = 4;
+            button1.Text = "PLAY";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.backgroundMenu;
+            pictureBox1.Location = new Point(-1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1012, 819);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Orange;
+            button2.FlatAppearance.BorderColor = Color.Brown;
+            button2.FlatAppearance.BorderSize = 3;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Comic Sans MS", 14F, FontStyle.Bold);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(390, 481);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(229, 80);
+            button2.TabIndex = 4;
+            button2.Text = "Rules";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(254, 223, 231);
+            ClientSize = new Size(1010, 815);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Butterfly Catching Game MOO ICT";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+    }
+}
---
+++ projeto 09 com menu/Butterfly-Catching-Game-MOO-ICT-master/Butterfly Catching Game MOO ICT/Form1.cs
@@ -0,0 +1,20 @@
+namespace Butterfly_Catching_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameWindow game = new GameWindow();
+            game.Show();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Escreva as regras aqui!!");
+        }
+    }
+}
